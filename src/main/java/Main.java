cbb1b37d1d3b737eee1dfcd2bbf4ba1a37fbec75
Roberto Bautista <robertobau45@gmail.com>
2022-04-04 13:19:23 -0600

    Removed comments from Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,27 +9,6 @@ public class Main {
 		MainMenu mainMenu = new MainMenu();
 		mainMenu.startGame();
 
-//		Pet aPet = new Mosquito();
-//		Pet bPet = new Beaver();
-//
-//		Pet[] teamA = new Pet[3];
-//		teamA[0] = new Beetle();
-//		teamA[1] = new Mosquito();
-//		teamA[2] = new Rat();
-//		Pet[] teamB = new Pet[3];
-//		teamB[0] = new Ant();
-//		teamB[1] = new Cricket();
-//		teamB[2] = new Dodo();
-
-//		IndividualFight fight = new IndividualFight(aPet, bPet);
-//		fight.startFight();
-//		System.out.println(fight.startFight());
-//		TeamsFight tFight = new TeamsFight(ArrayUtils.cloner(teamA), ArrayUtils.cloner(teamB));
-//		tFight.startTeamFight();
-//
-//		ArenaGame agame = new ArenaGame();
-//		agame.PlayArena();
-
 	}
 
 }
